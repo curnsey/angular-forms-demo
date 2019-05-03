48601f38ecf05d0ad0099944659ee862287a7dc5
Curnsey <curnsey.dev@gmail.com>
2019-05-03 08:38:19 -0400

    Field validation complete.

---
+++ src/app/data/user-settings.ts
@@ -0,0 +1,9 @@
+export interface UserSettings{
+    name: string,
+    dob: string,
+    password: string,
+    emailOffers: boolean,
+    backgroundColour: string,
+    subscriptionType: number,
+    notes: string
+}
--- src/app/user-settings/user-settings.component.ts
+++ src/app/user-settings/user-settings.component.ts
@@ -1,19 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings',
   templateUrl: './user-settings.component.html',
   styleUrls: ['./user-settings.component.css']
 })
+
 export class UserSettingsComponent implements OnInit {
+  originalUserSettings: UserSettings;
+  userSettings : UserSettings;
 
   constructor() { }
 
   ngOnInit() {
+    if(localStorage.getItem('userSettings')){
+      this.originalUserSettings = JSON.parse(localStorage.getItem('userSettings'));
+    }
+    else this.originalUserSettings = {
+        name: null,
+        dob: null,
+        emailOffers: null,
+        password: null,
+        backgroundColour: null,
+        subscriptionType: null,
+        notes: null
+      };
+
+    this.userSettings = { ...this.originalUserSettings };
+
   }
 
-  send(): void{
-    console.log("Sent!");
+  onBlur(field: NgModel){
+    console.log('in onBlur: ', field.valid);
   }
 
+  onSubmit(form: NgForm): void{
+    console.log('in onSubmit: ', form.valid);
+  }
 }
